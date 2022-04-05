bb4f1700a6afd3d7a9501e597aa71c4b29fb8787
Jethro Williams <jethrowilliams@outlook.com>
2022-04-05 15:10:56 +0100

    Install tailwind typography plugin

--- components/layout/main-content/articles/ArticleBody.js
+++ components/layout/main-content/articles/ArticleBody.js
@@ -1,9 +1,5 @@
 const ArticleBody = props => {
-  return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: props.content }} />
-    </div>
-  );
+  return <div className='prose max-w-none' dangerouslySetInnerHTML={{ __html: props.content }} />;
 };
 
 export default ArticleBody;

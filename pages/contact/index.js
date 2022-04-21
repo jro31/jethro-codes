a3050566c0bbc5d924d89b86de4ddb590617fdee
Jethro Williams <jethrowilliams@outlook.com>
2022-04-21 16:00:46 +0100

    Simplify template code slightly

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,65 +1,15 @@
+import Dots from './Dots';
+
+const inputClasses =
+  'py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md';
+const labelClasses = 'block text-sm font-medium text-gray-700';
+
 const Contact = () => {
   return (
     <div className='bg-gray-50 py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24'>
       <div className='relative max-w-xl mx-auto'>
-        <svg
-          className='absolute left-full transform translate-x-1/2'
-          width={404}
-          height={404}
-          fill='none'
-          viewBox='0 0 404 404'
-          aria-hidden='true'
-        >
-          <defs>
-            <pattern
-              id='85737c0e-0916-41d7-917f-596dc7edfa27'
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits='userSpaceOnUse'
-            >
-              <rect
-                x={0}
-                y={0}
-                width={4}
-                height={4}
-                className='text-gray-200'
-                fill='currentColor'
-              />
-            </pattern>
-          </defs>
-          <rect width={404} height={404} fill='url(#85737c0e-0916-41d7-917f-596dc7edfa27)' />
-        </svg>
-        <svg
-          className='absolute right-full bottom-0 transform -translate-x-1/2'
-          width={404}
-          height={404}
-          fill='none'
-          viewBox='0 0 404 404'
-          aria-hidden='true'
-        >
-          <defs>
-            <pattern
-              id='85737c0e-0916-41d7-917f-596dc7edfa27'
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits='userSpaceOnUse'
-            >
-              <rect
-                x={0}
-                y={0}
-                width={4}
-                height={4}
-                className='text-gray-200'
-                fill='currentColor'
-              />
-            </pattern>
-          </defs>
-          <rect width={404} height={404} fill='url(#85737c0e-0916-41d7-917f-596dc7edfa27)' />
-        </svg>
+        <Dots positionClasses='left-full' translateClass='translate-x-1/2' />
+        <Dots positionClasses='right-full bottom-0' translateClass='-translate-x-1/2' />
         <div className='text-center'>
           <h2 className='text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
             Get in touch
@@ -76,7 +26,7 @@ const Contact = () => {
             className='grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8'
           >
             <div>
-              <label htmlFor='first-name' className='block text-sm font-medium text-gray-700'>
+              <label htmlFor='first-name' className={labelClasses}>
                 First name
               </label>
               <div className='mt-1'>
@@ -85,12 +35,12 @@ const Contact = () => {
                   name='first-name'
                   id='first-name'
                   autoComplete='given-name'
-                  className='py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'
+                  className={inputClasses}
                 />
               </div>
             </div>
             <div>
-              <label htmlFor='last-name' className='block text-sm font-medium text-gray-700'>
+              <label htmlFor='last-name' className={labelClasses}>
                 Last name
               </label>
               <div className='mt-1'>
@@ -99,12 +49,12 @@ const Contact = () => {
                   name='last-name'
                   id='last-name'
                   autoComplete='family-name'
-                  className='py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'
+                  className={inputClasses}
                 />
               </div>
             </div>
             <div className='sm:col-span-2'>
-              <label htmlFor='email' className='block text-sm font-medium text-gray-700'>
+              <label htmlFor='email' className={labelClasses}>
                 Email
               </label>
               <div className='mt-1'>
@@ -113,12 +63,12 @@ const Contact = () => {
                   name='email'
                   type='email'
                   autoComplete='email'
-                  className='py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md'
+                  className={inputClasses}
                 />
               </div>
             </div>
             <div className='sm:col-span-2'>
-              <label htmlFor='message' className='block text-sm font-medium text-gray-700'>
+              <label htmlFor='message' className={labelClasses}>
                 Message
               </label>
               <div className='mt-1'>
@@ -126,7 +76,7 @@ const Contact = () => {
                   id='message'
                   name='message'
                   rows={4}
-                  className='py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md'
+                  className={`${inputClasses} border`}
                   defaultValue={''}
                 />
               </div>

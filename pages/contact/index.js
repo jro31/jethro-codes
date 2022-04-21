868b9bd89b9a1fe4719993b528be79ead6759006
Jethro Williams <jethrowilliams@outlook.com>
2022-04-21 17:21:42 +0100

    Successfully send first email

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,10 +1,71 @@
+import { useState } from 'react';
 import Dots from './Dots';
 
 const inputClasses =
   'py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md';
 const labelClasses = 'block text-sm font-medium text-gray-700';
 
+const successMessage = 'Email sent successfully!';
+
 const Contact = () => {
+  const [enteredFirstName, setEnteredFirstName] = useState('');
+  const [enteredLastName, setEnteredLastName] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredMessage, setEnteredMessage] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [sendStatus, setSendStatus] = useState('');
+
+  const formIsValid = () => {
+    return (
+      enteredFirstName.trim().length > 0 &&
+      enteredEmail.trim().length > 0 &&
+      enteredMessage.trim().length > 0
+    );
+  };
+
+  const resetForm = () => {
+    setEnteredFirstName('');
+    setEnteredLastName('');
+    setEnteredEmail('');
+    setEnteredMessage('');
+  };
+
+  const formSubmitHandler = async event => {
+    event.preventDefault();
+    setIsSubmitting(true);
+    setSendStatus('');
+
+    if (!formIsValid()) return;
+
+    try {
+      const response = await fetch('/api/sendgrid', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          enteredFirstName: enteredFirstName.trim(),
+          enteredLastName: enteredLastName.trim(),
+          enteredEmail: enteredEmail.trim(),
+          enteredMessage: enteredMessage.trim(),
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok && data.success) {
+        setSendStatus(successMessage);
+        setIsSubmitting(false);
+        resetForm();
+      } else {
+        throw new Error(data.error_message || 'Something went wrong. Please try again.');
+      }
+    } catch (error) {
+      setSendStatus(error.message);
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <div className='bg-gray-50 py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24'>
       <div className='relative max-w-xl mx-auto'>
@@ -21,13 +82,12 @@ const Contact = () => {
         </div>
         <div className='mt-12'>
           <form
-            action='#'
-            method='POST'
+            onSubmit={formSubmitHandler}
             className='grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8'
           >
             <div>
               <label htmlFor='first-name' className={labelClasses}>
-                First name
+                First name*
               </label>
               <div className='mt-1'>
                 <input
@@ -36,6 +96,8 @@ const Contact = () => {
                   id='first-name'
                   autoComplete='given-name'
                   className={inputClasses}
+                  value={enteredFirstName}
+                  onChange={event => setEnteredFirstName(event.target.value)}
                 />
               </div>
             </div>
@@ -50,12 +112,14 @@ const Contact = () => {
                   id='last-name'
                   autoComplete='family-name'
                   className={inputClasses}
+                  value={enteredLastName}
+                  onChange={event => setEnteredLastName(event.target.value)}
                 />
               </div>
             </div>
             <div className='sm:col-span-2'>
               <label htmlFor='email' className={labelClasses}>
-                Email
+                Email*
               </label>
               <div className='mt-1'>
                 <input
@@ -64,12 +128,14 @@ const Contact = () => {
                   type='email'
                   autoComplete='email'
                   className={inputClasses}
+                  value={enteredEmail}
+                  onChange={event => setEnteredEmail(event.target.value)}
                 />
               </div>
             </div>
             <div className='sm:col-span-2'>
               <label htmlFor='message' className={labelClasses}>
-                Message
+                Message*
               </label>
               <div className='mt-1'>
                 <textarea
@@ -77,11 +143,16 @@ const Contact = () => {
                   name='message'
                   rows={4}
                   className={`${inputClasses} border`}
-                  defaultValue={''}
+                  value={enteredMessage}
+                  onChange={event => setEnteredMessage(event.target.value)}
                 />
               </div>
             </div>
+            {/* TODO - Add 'success' message if message sent */}
+            {/* TODO - Add error message if not sent */}
             <div className='sm:col-span-2'>
+              {/* TODO - Disable button until valid */}
+              {/* TODO - Disable button and add loading spinner when isSubmitting */}
               <button
                 type='submit'
                 className='w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'

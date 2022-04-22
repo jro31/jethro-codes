ebdd8ad2aceaeddbbad3c2f4b0c0af545d57a9f6
Jethro Williams <jethrowilliams@outlook.com>
2022-04-22 11:11:49 +0100

    Fix code block issue
    Widths on mobile were becoming distorted by code blocks. Adding a max-width to the code block fixes this, although still an issue with long lines of unspaced inline code.

--- components/layout/main-content/article-container/ArticleBody.js
+++ components/layout/main-content/article-container/ArticleBody.js
@@ -1,9 +1,12 @@
+// FIXME - Does not work on mobile; the content doesn't shrink to fit the screen, and some of it is not viewable
+// Only on Meals of Change article, so perhaps related to size of photos, or code blocks
+
 const ArticleBody = props => {
   return (
     <section className='-mt-32 max-w-7xl mx-auto relative z-10 md:pb-6 sm:px-6 lg:px-8'>
       <div className='flex justify-center py-4 px-4 lg:py-8 sm:px-6 lg:px-8 bg-white rounded-lg shadow'>
         <div
-          className='prose 2xl:max-w-[900px] prose-img:m-0 prose-img:rounded-md prose-img:shadow-lg'
+          className='prose max-w-full 2xl:max-w-[900px] prose-img:m-0 prose-img:rounded-md prose-img:shadow-lg prose-pre:max-w-full'
           dangerouslySetInnerHTML={{ __html: props.content }}
         />
       </div>

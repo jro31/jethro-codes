4564cb45c580a2e139a86b1d36b8aececc01605c
Jethro Williams <jethrowilliams@outlook.com>
2022-04-25 11:27:01 +0100

    Remove Redux Toolkit

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,14 @@
 // TODO - Add sitemap to Google search console
 // ESSENTIAL - Update readme
 
-import { Provider } from 'react-redux';
-
-import store from '../store';
 import Layout from '../components/layout';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 };
 

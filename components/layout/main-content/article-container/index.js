abb064d1d96a21c7b17a9891eee0e75a08d8e1a0
Jethro Williams <jethrowilliams@outlook.com>
2022-04-25 12:31:58 +0100

    Add <Head> section to [section] page

--- components/layout/main-content/article-container/index.js
+++ components/layout/main-content/article-container/index.js
@@ -11,7 +11,7 @@ import { baseUrl, stockImagePath } from '../..';
 const ArticleContainer = ({ article }) => {
   const router = useRouter();
 
-  const metaTitle = `${article.title}${article.subtitle && ` | ${article.subtitle}`}`;
+  const pageTitle = `${article.title}${article.subtitle && ` | ${article.subtitle}`}`;
   const pageUrl = `${baseUrl}${router.asPath}`;
   const socialMediaImagePath = `${baseUrl}${
     article.coverImage ? article.coverImage : stockImagePath
@@ -21,19 +21,19 @@ const ArticleContainer = ({ article }) => {
     <article>
       <Head>
         {/* TODO - Once the app is online, run these tags through Twitter/Facebook tools, and check cover image and stock image paths work */}
-        <title>{metaTitle}</title>
+        <title>{pageTitle}</title>
         <meta name='description' content={article.description} />
         <meta name='keywords' content={article.tags || ''} />
 
         {/* Facebook */}
-        <meta property='og:title' content={metaTitle} />
+        <meta property='og:title' content={pageTitle} />
         <meta property='og:description' content={article.description} />
         <meta property='og:type' content='website' />
         <meta property='og:url' content={pageUrl} />
         <meta property='og:image' content={socialMediaImagePath} />
 
         {/* Twitter */}
-        <meta name='twitter:title' content={metaTitle} />
+        <meta name='twitter:title' content={pageTitle} />
         <meta name='twitter:site' content='@jethro_williams' />
         <meta name='twitter:description' content={article.description} />
         <meta name='twitter:image' content={socialMediaImagePath} />
--- pages/[section].js
+++ pages/[section].js
@@ -1,4 +1,5 @@
-// ESSENTIAL - Add <Head> section
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import { getArticles } from '../lib/api';
 import HorizontalCard from '../components/ui/HorizontalCard';
@@ -6,16 +7,48 @@ import HorizontalCardsContainer from '../components/ui/HorizontalCardsContainer'
 import SectionHome from '../components/layout/main-content/section-home';
 import { articleSections } from '../hooks/useSectionDetails';
 import useHeroImage from '../hooks/useHeroImage';
+import useActiveSection from '../hooks/useActiveSection';
+import useSectionDetails from '../hooks/useSectionDetails';
+import { baseUrl, stockImagePath } from '../components/layout';
 
 const Section = ({ allArticles }) => {
+  const router = useRouter();
+  const activeSection = useActiveSection();
+  const sectionDetails = useSectionDetails();
+
+  const pageTitle = `${sectionDetails(activeSection()).title} | jethro.codes`;
+  const pageDescription = sectionDetails(activeSection()).description;
+  const socialMediaImagePath = `${baseUrl}${stockImagePath}`;
+
   return (
-    <SectionHome heroImage={useHeroImage(allArticles)}>
-      <HorizontalCardsContainer>
-        {allArticles.map(article => (
-          <HorizontalCard key={`${article.title}-card`} cardDetails={article} />
-        ))}
-      </HorizontalCardsContainer>
-    </SectionHome>
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name='description' content={pageDescription} />
+        <meta name='keywords' content='' />
+
+        {/* Facebook */}
+        <meta property='og:title' content={pageTitle} />
+        <meta property='og:description' content={pageDescription} />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content={`${baseUrl}${router.asPath}`} />
+        <meta property='og:image' content={socialMediaImagePath} />
+
+        {/* Twitter */}
+        <meta name='twitter:title' content={pageTitle} />
+        <meta name='twitter:site' content='@jethro_williams' />
+        <meta name='twitter:description' content={pageDescription} />
+        <meta name='twitter:image' content={socialMediaImagePath} />
+        <meta name='twitter:card' content='summary_large_image' />
+      </Head>
+      <SectionHome heroImage={useHeroImage(allArticles)}>
+        <HorizontalCardsContainer>
+          {allArticles.map(article => (
+            <HorizontalCard key={`${article.title}-card`} cardDetails={article} />
+          ))}
+        </HorizontalCardsContainer>
+      </SectionHome>
+    </>
   );
 };
 
